c28bc85e31f992b6e5c40e08e03988df8e8c7d6a
Luis Felipe <2511550630@fema.edu.br>
2025-11-26 14:18:26 -0300

    feat: adiciona objeto com os temas dos dispositivos

---
+++ constants/Colors.js
@@ -0,0 +1,19 @@
+const tintColorLight = "#eaeaea";
+const tintColorDark = "#313131";
+
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#ffffff",
+    tint: tintColorLight,
+    tabIconDefault: "#5a5a5a",
+    tabIconSelected: "#000000",
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    tabIconSelected: "#e1e1e1",
+    tabIconDefault: "#6f6f6f",
+  },
+};
